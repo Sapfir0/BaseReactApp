433082e9b21ac0f4241b0ed50b8d8ffb11e7a321
Sapfir0 <sapfir999999@yandex.ru>
2021-07-19 21:24:29 +0300

    Webpack exists in base build now

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">See it</header>
+            <header className="App-header">See it</header>f fas
         </div>
     );
 }
