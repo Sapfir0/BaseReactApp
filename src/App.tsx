b4e5a28e51ac94b82c8ee65636460839eaf06959
Sapfir0 <sapfir999999@yandex.ru>
2021-07-19 22:37:21 +0300

    Added gh actions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">See it</header>f fas
+            <header className="App-header">See it</header>
         </div>
     );
 }
