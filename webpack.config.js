568140cf548b93ecdcb0c140c08d313ab3043cdb
Sapfir0 <sapfr999999@yandex.ru>
2022-03-25 11:19:55 +0300

    Changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,32 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 const path = require('path');
-const fs = require('fs');
-const dotenv = require('dotenv');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
-const root = path.resolve('.');
+const APP_NAME = '/appName/';
 
 module.exports = (env) => {
-    const deployedAppName = '/appName/';
-    console.log(env);
-
     const currentEnivronment = env.NODE_ENV || env.nodeEnv; // почему-то devServer и обычная сборка по-разному прокидывают аргументы
-    const isProduction = currentEnivronment === 'prod';
 
-    const basePath = root + '/.env';
+    const isProduction = currentEnivronment === 'production';
 
-    const envPath = basePath + '.' + currentEnivronment;
+    const publicPath = isProduction ? APP_NAME : '/';
+    console.log('Public path:', publicPath);
+    console.log('cli env: ', env);
 
-    const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-    const fileEnv = dotenv.config({ path: finalPath }).parsed;
-
-    const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-        prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-        return prev;
-    }, {});
     const devtool = isProduction ? false : 'eval-cheap-module-source-map'; // false или строка по шаблону
-    console.log(envKeys);
     const productionPlugins = [];
+    if (isProduction) {
+        productionPlugins.push(new MiniCssExtractPlugin());
+    }
 
     return {
         entry: './src/index.tsx',
         output: {
             path: path.resolve(__dirname, 'dist'),
-            publicPath: isProduction ? deployedAppName : '/', // этот путь будет добавляться в пути до каждого бандла внутри html и других бандлов
+            publicPath: publicPath, // этот путь будет добавляться в пути до каждого бандла внутри html и других бандлов
             filename: 'js/[name].[fullhash].bundle.js',
             chunkFilename: 'js/[name].[fullhash].bundle.js',
         },
@@ -79,13 +71,18 @@ module.exports = (env) => {
                         },
                     ],
                 },
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                        },
+                    ],
+                },
             ],
         },
         devServer: {
-            contentBase: path.join(__dirname, 'public'),
             port: 3000,
-            watchContentBase: true,
-            progress: true,
             compress: true,
             hot: true,
             historyApiFallback: true,
@@ -93,7 +90,10 @@ module.exports = (env) => {
         plugins: [
             new CleanWebpackPlugin(),
             ...productionPlugins,
-            new webpack.DefinePlugin(envKeys),
+            new Dotenv({ path: `.env.${currentEnivronment}`, publicPath }),
+            new CopyPlugin({
+                patterns: [{ from: 'public', to: 'public' }],
+            }),
             new HtmlWebpackPlugin({ template: './public/index.html', filename: 'index.html', inject: 'body' }),
         ],
     };
